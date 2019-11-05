fd44f4357f8bec9763002900d4f5746a24bd6b75
AlbertoTakahashi <56005968+AlbertoTakahashi@users.noreply.github.com>
2019-11-05 00:40:20 -0300

    Update objectModelBean.java
    teste

--- objectModelBean.java
+++ objectModelBean.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ManagedBean;
 
 import javax.inject.Named;
@@ -13,10 +9,7 @@ import java.util.*;
 import java.util.logging.*;
 import javax.json.*;
 import javax.json.stream.JsonGenerator;
-/**
- *
- * @author alber
- */
+
 @Named(value = "objectModelBean")
 @SessionScoped
 public class objectModelBean implements Serializable {
